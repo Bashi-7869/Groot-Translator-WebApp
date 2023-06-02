f3462c985b9a3d3fbd7d22ffe0b17ed3c3ffd74b
Bashi-7869 <121685218+Bashi-7869@users.noreply.github.com>
2023-06-02 22:25:55 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-let userName = prompt("Enter User Name")
-let wellcomeMessage = "wellcome to java script " + userName
-alert(wellcomeMessage)
+// let userName = prompt("Enter User Name")
+// let wellcomeMessage = "wellcome to java script " + userName
+// alert(wellcomeMessage)
