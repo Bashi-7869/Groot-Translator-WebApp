15d5543fb5bc2a73fabaa58d799bbede384dd63c
Bashi-7869 <121685218+Bashi-7869@users.noreply.github.com>
2023-06-20 11:03:02 +0530

    storing URL

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 // let userName = prompt("Enter User Name")
 // let wellcomeMessage = "wellcome to java script " + userName
 // alert(wellcomeMessage)
+
+const textInput = document.querySelector('#textinput');
+const button = document.querySelector('#button');
+const outputDiv = document.querySelector('#output');
+
+// console.log(outputDiv);
+//   storing url
+let serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+//converting text and addding queryperams and textkey to server URL
+function getserverURL(text){
+   return serverUrl + "?" + "text=" + text
+}
+
+
+   function clickHandler(){
+
+      // taking input from user
+       let inputText = textInput.value;
+
+      //  processing dsta
+       fetch(getserverURL(inputText))
+       .then(rsponse => rsponse.json())
+       .then (json =>{let translatedText = json.contents.translated;
+              outputDiv.innerText = translatedText;            // Showing the output to the user
+      })
+    
+   }
+
+button.addEventListener('click', clickHandler);
